1677b4b61b215fa77508d6956883989fc74b3b5d
masome hosine <hosine.masome@gmail.com>
2024-01-07 02:45:08 +0330

    adde DarkMode class

---
+++ src/layout/SiteLayout.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import React from 'react'
+
+function SiteLayout({ children }) {
+  return (
+    <div className=" flex w-full flex-col items-center justify-center gap-2 text-black  dark:bg-gray-800 lg:gap-6 ">
+      <Header />
+      <div className="fadeShow w-full ">{children}</div>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default SiteLayout
