9c4134475e929843e77fc42c3cfa5676b1766434
masome hosine <hosine.masome@gmail.com>
2024-01-08 02:39:10 +0330

    create submit button component in header

---
+++ src/components/HeaderComponents/SubmitButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function SubmitButton() {
+  return (
+    <Link to="/sginUp">
+      <div className="flex items-center justify-center">
+        <button className=" rounded-lg border border-violet-600 px-4 py-2 dark:border-violet-500 dark:border-2 font-EstedadFont">
+          ورود | ثبت نام
+        </button>
+      </div>
+    </Link>
+  )
+}
+
+export default SubmitButton
